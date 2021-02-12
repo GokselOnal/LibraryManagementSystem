9d303d95e2f5357db495a019e8a1b7f45468a807
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-02-12 17:05:29 +0300

    Initial commit

---
+++ src/main/java/edu/ozu/cs202project/GokselcanOnalCs202ProjectApplication.java
@@ -0,0 +1,12 @@
+package edu.ozu.cs202project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GokselcanOnalCs202ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokselcanOnalCs202ProjectApplication.class, args);
+	}
+}
